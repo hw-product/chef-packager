81c23eaf79f950e382c14fb59a3c5e32c98e90c1
Chris Roberts <chrisroberts.code@gmail.com>
2013-11-01 11:35:19 -0700

    Remove deprecated recipes

--- recipes/callbacks.rb
+++
@@ -1,8 +0,0 @@
-node[:fission][:callbacks].each do |name, config|
-  case config[:type].to_sym
-  when :fission
-  when :webservice
-  else
-    Chef::Log.error "Fission callback: Unknown callback type provided: #{config[:type]}"
-  end
-end
--- recipes/pre_build.rb
+++
@@ -1,9 +0,0 @@
-node[:fission][:build][:commands][:before][:build].each do |command|
-  execute "pre_build(#{command})" do
-    command command
-    cwd '/tmp'
-  end
-  not_if do
-    node[:fission][:internal_overrides][:disable][:commands][:before][:build]
-  end
-end
